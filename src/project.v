de149758be8606d128b5dec2ba435f6f017c0c85
Laniakea67 <144993761+Laniakea67@users.noreply.github.com>
2026-08-01 07:52:21 +0000

    gekeken hoeveel oppervlakte nodig is voor geheugen

--- src/project.v
+++ src/project.v
@@ -16,12 +16,26 @@ module tt_um_conwaysgameoflife (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  parameter int M = 32;
+  parameter int N = 16;
+  
+  reg [M-1:0][N-1:0] grid;
+
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[0] = grid[ui_in[4:0]][uio_in[3:0]];  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7:4] = {ui_in[7:5], 1'b0} + uio_in[7:4];
+  assign uo_out[3:1] = 0;
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, rst_n, 1'b0};
+
+  always @(posedge clk) begin
+    if(ena)
+      grid[ui_in[4:0]][uio_in[3:0]] <= !grid[ui_in[4:0]][uio_in[3:0]];
+  end
+
 
 endmodule
