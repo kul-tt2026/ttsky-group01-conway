a7a10cd0d505be09b3f73c74d7eed28d6172af60
Laniakea67 <144993761+Laniakea67@users.noreply.github.com>
2026-08-06 11:32:45 +0000

    register board licht aangepast

--- src/register_board/register_board.sv
+++ src/register_board/register_board.sv
@@ -1,54 +1,77 @@
 // input 
 // reset_n == global reset
 // data_in == pixel black or white (0 or 1)
-// read_address == x and y coordinate for pixel that needs to be displayed
-// write_address == x and y coordinate where new value gets saved
+// read_address == row and col coordinate for pixel that needs to be displayed
+// write_address == row and col coordinate where new value gets saved
 // write_enable == 1 if you want to write to a board
-// active_board selcts which board is currently displayed (defined by logic)
+// active_board_read selects which board you read
+// active_board_write selects which board you write to
+
 // output 
 // data_out == value of pixel from read address
 // general: logic can write to a board, when its full active_board bitflips. The board thats just been written changes to read
 // and the old read board can get new data written onto it.
 // testing via cd src, cd register_board, make simulate in terminal 
+
+/*
+Origineel geschreven door Sander, licht aangepast door Sieben
+Aanpassingen Sieben:
+ - gezorgd dat je van één bord kunt lezen terwijl je op het andere schrijft
+ - gezorgd dat je een ander aantal rijen en kolommen kan hebben
+ - x en y hernoemd naar row en col
+*/
+
+
+`default_nettype none
 `timescale 1ns/1ps
-module register_board #(parameter BOARD_SIZE = 8) (
-    input clk,
-    input reset_n,
-    input data_in,
-    input [2:0] read_address_x,
-    input [2:0] read_address_y,
-    input [2:0] write_address_x,
-    input [2:0] write_address_y,
-    input active_board,
-    input write_enable,
-    output data_out
+module register_board #(
+    parameter int row_count = 8,  // aantal pirowels
+    parameter int col_count = 8   // Werkt niet voor row_count of col_count = 1    
+) (
+    input logic clk,
+    input logic reset_n,
+    input logic data_in,
+    input logic [row_bits - 1:0] read_address_row,
+    input logic [col_bits - 1:0] read_address_col,
+    input logic [row_bits - 1:0] write_address_row,
+    input logic [col_bits - 1:0] write_address_col,
+    input logic active_board_read,
+    input logic active_board_write,
+    input logic write_enable,
+    output logic data_out
 );
 
-reg board0 [0:BOARD_SIZE-1][0:BOARD_SIZE-1];
-reg board1 [0:BOARD_SIZE-1][0:BOARD_SIZE-1];
+    localparam int row_bits = $clog2(row_count);
+    localparam int col_bits = $clog2(col_count);
 
-integer x;
-integer y;
+    reg board0 [row_count - 1:0][col_count - 1:0]; // grid
+    reg board1 [row_count - 1:0][col_count - 1:0]; // previous grid
 
-always @(posedge clk or negedge reset_n) begin 
-    if (!reset_n) begin
-        for (x=0; x<BOARD_SIZE; x++) begin
-            for (y=0; y<BOARD_SIZE; y++) begin 
-                board0[y][x] <= 1'b0;
-                board1[y][x] <= 1'b0;
+    integer row;
+    integer col;
+
+    always_ff @(posedge clk or negedge reset_n) begin 
+        if (!reset_n) begin
+            for (row=0; row < row_count; row++) begin
+                for (col=0; col < col_count; col++) begin 
+                    board0[row][col] <= 1'b0;
+                    board1[row][col] <= 1'b0;
+                end
+            end
+        end
+        else begin
+            if (write_enable) begin
+                if (!active_board_write) 
+                    board0[write_address_row][write_address_col] <= data_in;
+                else
+                    board1[write_address_row][write_address_col] <= data_in;
             end
         end
     end
-    else begin
-        if (write_enable) begin
-            if (active_board) 
-                board0[write_address_y][write_address_x] <= data_in;
-            else
-                board1[write_address_y][write_address_x] <= data_in;
-        end
-    end
-end
 
-assign data_out = active_board ? board1[read_address_y][read_address_x]:board0[read_address_y][read_address_x];
+    // combinatorisch lezen
+    assign data_out = active_board_read ? board1[read_address_row][read_address_col] : board0[read_address_row][read_address_col];
 
 endmodule
+
+`default_nettype wire
--- src/register_board/tb_register_board.sv
+++ src/register_board/tb_register_board.sv
@@ -1,36 +1,54 @@
 `timescale 1ns/1ps
 module tb_register_board ();
-    reg clk, reset_n, data_in, active_board, write_enable;
-    reg [2:0] read_address_x, read_address_y, write_address_x, write_address_y;
+
+    localparam int ROWS = 8;
+    localparam int COLS = 8;
+
+    reg clk, reset_n, data_in, active_board_read, active_board_write, write_enable;
+    reg [$clog2(ROWS)-1:0] read_address_row, write_address_row;
+    reg [$clog2(COLS)-1:0] read_address_col, write_address_col;
     wire data_out;
 
-        register_board board_reg (
-            .clk(clk),
-            .reset_n(reset_n),
-            .data_in(data_in),
-            .active_board(active_board),
-            .write_enable(write_enable),
-            .read_address_x(read_address_x),
-            .read_address_y(read_address_y),
-            .write_address_x(write_address_x),
-            .write_address_y(write_address_y),
-            .data_out(data_out)
-        );
+    register_board #(
+        .row_count(ROWS),
+        .col_count(COLS)
+    ) board_reg (
+        .clk(clk),
+        .reset_n(reset_n),
+        .data_in(data_in),
+        .active_board_read(active_board_read),
+        .active_board_write(active_board_write),
+        .write_enable(write_enable),
+        .read_address_row(read_address_row),
+        .read_address_col(read_address_col),
+        .write_address_row(write_address_row),
+        .write_address_col(write_address_col),
+        .data_out(data_out)
+    );
+
     always begin 
         #1 clk = ~clk;
     end
+
+    // Waveform
+    initial begin
+        $dumpfile("tb_register_board.vcd");
+        $dumpvars(0, tb_register_board);
+    end
+
     initial begin
         clk = 1;
         reset_n = 1;
         #5;
         reset_n = 0;
         write_enable = 0;
-        active_board = 0;
+        active_board_read = 0;
+        active_board_write = 0;
         data_in = 0;
-        read_address_x = 0;
-        read_address_y = 0;
-        write_address_x = 0;
-        write_address_y = 0;
+        read_address_row = 0;
+        read_address_col = 0;
+        write_address_row = 0;
+        write_address_col = 0;
         #5;
         // test reset_n
         reset_n = 1;
@@ -42,81 +60,85 @@ module tb_register_board ();
             $display("passed");
         #2;
         // test writing 1 coordinate
-        write_address_x = 3;
-        write_address_y = 4;
+        write_address_row = 3;
+        write_address_col = 4;
+        active_board_write = 1;
         data_in = 1;
         @(posedge clk);
         #2;
-        active_board = 1;
-        read_address_x = 3;
-        read_address_y = 4;
+        active_board_read = 1;
+        read_address_row = 3;
+        read_address_col = 4;
         #2;
         if (data_out)
             $display("passed");
         else
             $display("expected 1 received %b", data_out);
         #2;
-        write_address_x = 1;
-        write_address_y = 1;
+        active_board_write = 0;
+        write_address_row = 1;
+        write_address_col = 1;
         data_in = 1;
         @(posedge clk);
-        write_address_x = 3;
-        write_address_y = 4;
+        #2        
+        write_address_row = 3;
+        write_address_col = 4;
         data_in = 1;
         #2;
-        active_board = 0;
-        read_address_x = 3;
-        read_address_y = 4;
+        active_board_read = 0;
+        read_address_row = 3;
+        read_address_col = 4;
         #2;
         if (data_out)
             $display("passed");
         else
             $display("expected 1 received %b", data_out);
-        read_address_x = 1;
-        read_address_y = 1;
-        #2;
-        if (data_out)
-            $display("passed");
-        else
-            $display("expected 1 received %b", data_out);
-        #2;
-        read_address_x = 7;
-        read_address_y = 7;
-        #2;
-        if (~data_out)
-            $display("passed");
-        else
-            $display("expected 0 received %b", data_out);
-        #2;
-        read_address_x = 7;
-        read_address_y = 0;
-        #2;
-        if (~data_out)
-            $display("passed");
-        else
-            $display("expected 0 received %b", data_out);
-        #2;
-        active_board = 0;
-        #2;
-        read_address_x = 0;
-        read_address_y = 7;
-        #2;
-        if (~data_out)
-            $display("passed");
-        else
-            $display("expected 0 received %b", data_out);
-        #2;
-        read_address_x = 3;
-        read_address_y = 4;
+        read_address_row = 1;
+        read_address_col = 1;
         #2;
         if (data_out)
             $display("passed");
         else
             $display("expected 1 received %b", data_out);
         #2;
-        active_board = 1;
-        read_address_x = 3;
-        read_address_y = 4;
+        write_enable = 0;
+        read_address_row = 7;
+        read_address_col = 7;
+        #2;
+        if (~data_out)
+            $display("passed");
+        else
+            $display("expected 0 received %b", data_out);
+        #2;
+        read_address_row = 7;
+        read_address_col = 0;
+        #2;
+        if (~data_out)
+            $display("passed");
+        else
+            $display("expected 0 received %b", data_out);
+        #2;
+        active_board_read = 0;
+        #2;
+        read_address_row = 0;
+        read_address_col = 7;
+        #2;
+        if (~data_out)
+            $display("passed");
+        else
+            $display("expected 0 received %b", data_out);
+        #2;
+        read_address_row = 3;
+        read_address_col = 4;
+        #2;
+        if (data_out)
+            $display("passed");
+        else
+            $display("expected 1 received %b", data_out);
+        #2;
+        active_board_read = 1;
+        read_address_row = 3;
+        read_address_col = 4;
         #2;
         if (data_out)
             $display("passed");
@@ -125,17 +147,17 @@ module tb_register_board ();
         #2;
         reset_n = 0;
         #2;
-        read_address_x = 3;
-        read_address_y = 4;
+        read_address_row = 3;
+        read_address_col = 4;
         #2;
         if (~data_out)
             $display("passed");
         else
             $display("expected 0 received %b", data_out);
         #2;
-        active_board = 0;
-        read_address_x = 0;
-        read_address_y = 7;
+        active_board_read = 0;
+        read_address_row = 0;
+        read_address_col = 7;
         #2;
         if (~data_out)
             $display("passed");
